3280c3985886c3a7e4bd54081f8f3bf7c5d9efd3
Jacek Kopecky <jacek@jacek.cz>
2018-03-08 14:55:18 +0000

    add a simple web page, allow disabling https

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const config = require('./config');
 const app = express();
 
 // routes can go here
-app.use('/', express.static('webpages', { extensions: ['html'] }));
+app.use('/', express.static('static', { extensions: ['html'] }));
 
 // starting the server
 // using mainly HTTPS; with HTTP redirecting to that
@@ -22,24 +22,32 @@ app.use('/', express.static('webpages', { extensions: ['html'] }));
 const port = process.env.PORT || config.port;
 const httpsPort = process.env.HTTPSPORT || config.httpsPort;
 
-const credentials = {};
-try {
-  credentials.key = fs.readFileSync(config.httpsKey, 'utf8');
-  credentials.cert = fs.readFileSync(config.httpsCert, 'utf8');
-} catch (e) {
-  console.error('ERROR: cannot open HTTPS credentials');
-  console.error(e);
-  process.exit(1);
-}
-
-https.createServer(credentials, app).listen(httpsPort, () => {
-  console.log(`Server listening on HTTPS port ${httpsPort}`);
-
-  // HTTP app will just redirect to HTTPS
-  const redirectApp = express();
-  redirectApp.get('*', (req, res) => res.redirect('https://' + req.hostname + req.url));
-
-  http.createServer(redirectApp).listen(port, () => {
-    console.log(`HTTP redirect server listening on port ${port}`);
+if (process.env.DISABLE_HTTPS) {
+  // start server without HTTPS
+  http.createServer(app).listen(port, () => {
+    console.log(`SERVER WITHOUT HTTPS running on port ${port}`);
   });
-});
+} else {
+  // start server with HTTPS
+  const credentials = {};
+  try {
+    credentials.key = fs.readFileSync(config.httpsKey, 'utf8');
+    credentials.cert = fs.readFileSync(config.httpsCert, 'utf8');
+  } catch (e) {
+    console.error('ERROR: cannot open HTTPS credentials');
+    console.error(e);
+    process.exit(1);
+  }
+
+  https.createServer(credentials, app).listen(httpsPort, () => {
+    console.log(`Server listening on HTTPS port ${httpsPort}`);
+
+    // HTTP app will just redirect to HTTPS
+    const redirectApp = express();
+    redirectApp.get('*', (req, res) => res.redirect('https://' + req.hostname + req.url));
+
+    http.createServer(redirectApp).listen(port, () => {
+      console.log(`HTTP redirect server listening on port ${port}`);
+    });
+  });
+}
